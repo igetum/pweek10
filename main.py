144c93a2f093e5bcc43d7079d3f978b2d6c34ce2
Antone B. Cabral <antone.b.cabral@gmail.com>
2020-05-12 06:28:48 +0000

    update commits and add a reloop feature

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+Antone Cabral
+5/11/2020
+
+Assignment Week 10
+
+Filename: main.py
+
+Purpose:
+This program takes in user name, phone, and addresses as stores the information in the directory and file of their choosing.
+"""
+
 import os
 
 
@@ -6,8 +18,10 @@ def main():
   print("Contact Data Store")
   print("------------------------------------")
 
+  #Application Loop
   while (True):
 
+    #Information Input Loop
     while (True):
       user_name = input("\nPlease enter your name: ")
       user_address = input("Please enter your address: ")
@@ -15,16 +29,18 @@ def main():
 
       print("\nYou have entered {}, {}, {}.".format(user_name, user_address, user_phone))
 
+      #Asks users if information is correct, breaks out otherwise
       qEntryVerify = input("\nIs the information you entered accurate? y or n: ")
-
+    
       if qEntryVerify == "y":
         break;
 
+    #Asks users if they want to store that data
     qStore = input("\nStore data? y or n: ")
 
-    
     if qStore == "y":
 
+      #Path input Loop
       while (True):
         path = os.getcwd()
         print("\nWhere would you like to store this data?")
@@ -38,27 +54,35 @@ def main():
         else:
           userPath = input("\nEnter path to the directory you want to save the file to: ")
     
-          try:
+          try: #tries and change directory to path, if it does not exist then it I ask to create the directory.
             os.chdir(userPath)
           except:
             print("Path does not exists.")
             qCreateDir = input("Do you want to create the new directory? y or no: ")
 
             if qCreateDir == "y":
+
+              #Makes directory and changes path of console to that path
               os.mkdir(userPath)
               os.chdir(userPath)
               break
 
+      #Write content to file passing in path and user information.
       writeDataToFile(path, user_name, user_address, user_phone)
-      break
 
-    elif (qStore == "n"):
+
+    runApp = input("\nEnter more data? y or n: ")
+    if runApp == "n":
       break
+    
+    continue
+
 
     
   print("\nExisting Program...")
         
   
+#writeDataToFile takes the path and user information and writes to file.
 def writeDataToFile(path, name, address, phone):
 
   userFileName = input("\nEnter filename: ")
