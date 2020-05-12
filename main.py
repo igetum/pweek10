20448ee7d61ef2f926faea36517ac6266bc024ab
Antone B. Cabral <antone.b.cabral@gmail.com>
2020-05-12 06:44:43 +0000

    modified userview in console, clean up outputs

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import os
 
 def main():
 
+  path = os.getcwd()
+  filename = ""
+
   print("Contact Data Store")
   print("------------------------------------")
 
@@ -27,7 +30,7 @@ def main():
       user_address = input("Please enter your address: ")
       user_phone = input("Please enter your phone number: ")
 
-      print("\nYou have entered {}, {}, {}.".format(user_name, user_address, user_phone))
+      print("\n\tYou have entered {}, {}, {}.".format(user_name, user_address, user_phone))
 
       #Asks users if information is correct, breaks out otherwise
       qEntryVerify = input("\nIs the information you entered accurate? y or n: ")
@@ -44,7 +47,7 @@ def main():
       while (True):
         path = os.getcwd()
         print("\nWhere would you like to store this data?")
-        print("Current directory: {}".format(path))
+        print("\tCurrent directory: {}".format(path))
 
         qDirVerify = input("\nSave in current directory? y or n: ")
 
@@ -57,19 +60,21 @@ def main():
           try: #tries and change directory to path, if it does not exist then it I ask to create the directory.
             os.chdir(userPath)
           except:
-            print("Path does not exists.")
-            qCreateDir = input("Do you want to create the new directory? y or no: ")
+            print("\n\tPath does not exists.")
+            qCreateDir = input("\nDo you want to create the new directory? y or no: ")
 
             if qCreateDir == "y":
 
               #Makes directory and changes path of console to that path
               os.mkdir(userPath)
               os.chdir(userPath)
+              path = userPath
               break
 
       #Write content to file passing in path and user information.
-      writeDataToFile(path, user_name, user_address, user_phone)
+      filepath = writeDataToFile(path, user_name, user_address, user_phone)
 
+      readFile(filepath)
 
     runApp = input("\nEnter more data? y or n: ")
     if runApp == "n":
@@ -77,9 +82,9 @@ def main():
     
     continue
 
-
+  
     
-  print("\nExisting Program...")
+  print("\n\tExisting Program...")
         
   
 #writeDataToFile takes the path and user information and writes to file.
@@ -91,8 +96,18 @@ def writeDataToFile(path, name, address, phone):
     f.write("{},{},{}\n".format(name, address, phone))
   f.close()
 
-  print("\nData stored successfully!")
+  print("\n\tData stored successfully!")
+  return path + "/" + userFileName
       
+#writeDataToFile takes the path and user information and writes to file.
+def readFile(path):
+
+  with open(path, 'r') as f:
+    print("\n\nData in {}".format(path))
+    print("---------------------------------------")
+    for line in f:
+      print("\t"+line, end='')
+    print("---------------------------------------")
 
 if __name__ == "__main__":
   main()
